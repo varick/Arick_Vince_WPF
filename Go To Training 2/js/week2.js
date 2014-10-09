bdc619a094fc75e67663883bf5d9c6288eb7f1c0
varick <varick@fullsail.edu>
2014-10-08 22:13:38 -0400

    Finished Example
    Future Age

--- Go To Training 2/js/week2.js
+++ Go To Training 2/js/week2.js
@@ -12,7 +12,7 @@ Go To Training Week 2
 //Ask the user their name
 var name = prompt("Please type in your name:");
 
-alert("Welcome "+name+"! Let's go ahead and figure out how old you are");
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are.");
 //Ask user what year they were born in
 //Create a variable to catch their answer
 
@@ -31,3 +31,19 @@ var age =currentYear - yearBorn;
 //Alert user with their age
 
 alert(name+ " you are "+age+" years old.");
+
+//Let's make this more complex
+//How old will the user be in the future
+
+//Ask the user, how many years in the future they would like to know
+var yearsMore = prompt ("How many years in the future would you like to know?");
+console.log(yearsMore);
+
+//Calculate our future age
+var futureAge = age + Number(yearsMore) ;
+console.log (futureAge);
+
+//Make results look pretty
+
+console.log("You will be "+futureAge+" in "+ yearsMore + "years.");
+alert("You will be "+futureAge+" in "+ yearsMore + "years.");
