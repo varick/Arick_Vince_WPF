de53fded5dc33f9fa3cfd6f12b2d01964000e9cc
varick <varick@fullsail.edu>
2014-10-09 16:27:00 -0400

    Total cups of coffee you drink in a year
    Total cups of coffee you drink in a year

--- Expressions_Personal/js/expressionsPersonal.js
+++ Expressions_Personal/js/expressionsPersonal.js
@@ -4,3 +4,12 @@ WPF 1410 Section 01
 Expressions_Personal
 10-09-2014
  */
+
+alert ("Welcome, Let's find out how many cups of coffee you will drink in a year?");
+
+var coffeeDaily = prompt ("How many cups do you drink daily?");
+
+var total = coffeeDaily * 365;
+
+alert ("You will drink " +total+ " cups of coffee in a year!");
+
