7dfb1f5a6e5301c5621e51da9ed8598e95218e22
varick <varick@fullsail.edu>
2014-10-08 21:39:39 -0400

    Asked the user for name and year
    Asked the user for name and year

--- Go To Training 2/js/week2.js
+++ Go To Training 2/js/week2.js
@@ -5,4 +5,16 @@ Go To Training Week 2
 10-08-2014
  */
 
-alert("Testing 1,2,3,");
+//alert("Testing 1,2,3,");
+
+//Create a js file that will tell us how old we are
+
+//Ask the user their name
+var name = prompt("Please type in your name:");
+
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are");
+//Ask user what year they were born in
+//Create a variable to catch their answer
+
+var yearBorn = prompt ("What year were you born?");
+console.log (yearBorn);
