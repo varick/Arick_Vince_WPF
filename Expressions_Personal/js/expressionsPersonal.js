53a1e4b821c2d800a1498313f0c406cb0be34bf2
varick <varick@fullsail.edu>
2014-10-09 16:53:44 -0400

    Added Console Log to Personal
    Added Console Log to Personal

--- Expressions_Personal/js/expressionsPersonal.js
+++ Expressions_Personal/js/expressionsPersonal.js
@@ -11,5 +11,12 @@ var coffeeDaily = prompt ("How many cups do you drink daily?");
 
 var total = coffeeDaily * 365;
 
-alert ("You will drink " +total+ " cups of coffee in a year!");
+var cupsYear = alert ("You will drink " +total+ " cups of coffee in a year!");
 
+var years = prompt ("How many years have you been drinking coffee?");
+
+var totalLife = (years * total);
+
+alert ("You have drank "+totalLife+" in your life so far.");
+
+console.log (totalLife);
