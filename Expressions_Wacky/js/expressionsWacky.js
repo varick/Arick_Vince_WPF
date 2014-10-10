4f38561d2edf885dee5f3160612f711bf1173890
varick <varick@fullsail.edu>
2014-10-09 23:58:44 -0400

    Total Amount of Candy
    Total Amount of Candy

--- Expressions_Wacky/js/expressionsWacky.js
+++ Expressions_Wacky/js/expressionsWacky.js
@@ -10,5 +10,13 @@ alert ("Happy Halloween how much candy should you get for your Trick or Treaters
 var lastYearChildren = prompt ("How many Trick or Treaters came last year?");
 console.log (lastYearChildren);
 
-var grownup = ("How many were to old Trick or Treating, and won't come this year?");
-console.log (grownup)
+var grownup = prompt ("How many were to old Trick or Treating, and won't come this year?");
+console.log (grownup);
+
+var candy = prompt ("What was the average amount of candy a Trick or Treater took?");
+console.log (candy)
+
+var totalCandy = (lastYearChildren - grownup * candy);
+
+alert ("You should get " +totalCandy+ "pieces of candy this year.");
+
