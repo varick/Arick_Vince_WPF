771b604cd268e84eedcb85c6309319092f883f6e
varick <varick@fullsail.edu>
2014-10-10 10:42:45 -0400

    Finished Prompt and Alert Assignment
    Finished Prompt and Alert Assignment

--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -4,5 +4,8 @@
 Prompt and Alert
  10-09-2014
  */
-var userInput = prompt("Enter your year of birth?");
-console.log (userInput);
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height?");
+var area = width * height;
+//console.log(area);
+alert("The area of rectanlge is " + area + " sq feet");
