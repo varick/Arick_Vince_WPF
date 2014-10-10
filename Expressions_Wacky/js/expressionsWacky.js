b3f37ed1349771978197dcfbd13e3352915a0462
varick <varick@fullsail.edu>
2014-10-09 23:50:55 -0400

    Trick or Treaters
    How much candy for Trick or Treaters

--- Expressions_Wacky/js/expressionsWacky.js
+++ Expressions_Wacky/js/expressionsWacky.js
@@ -5,4 +5,10 @@
  10-09-2014
  */
 //Welcome
-alert ("")
+alert ("Happy Halloween how much candy should you get for your Trick or Treaters?")
+
+var lastYearChildren = prompt ("How many Trick or Treaters came last year?");
+console.log (lastYearChildren);
+
+var grownup = ("How many were to old Trick or Treating, and won't come this year?");
+console.log (grownup)
