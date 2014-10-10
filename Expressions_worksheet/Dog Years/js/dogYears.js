8d33c0204d738271b20085b647a7735066f4c0fa
varick <varick@fullsail.edu>
2014-10-10 11:15:56 -0400

    Sparky is 21 years old in dog years
    Sparky is 21 years old in dog years

---
+++ Expressions_worksheet/Dog Years/js/dogYears.js
@@ -0,0 +1,18 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Dog Years
+ 10-09-2014
+ */
+//Question
+alert ("How old is Sparky in dog years?");
+
+//sparky age
+var sparkyHuman = prompt ("How many years old is Sparky in human years?");
+console.log (sparkyHuman);
+
+//multiply sparky age times 7, alert how old in dog years
+alert ("Sparky is " + sparkyHuman * 7 + " in dog years.");
+
+
+
--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -1,11 +1,8 @@
 /*
  Vince Arick
  WPF 1410 Section 01
-Prompt and Alert
- 10-09-2014
+ Dog Years
+ 10-10-2014
  */
-var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
-var height = prompt("Please enter the height?");
-var area = width * height;
-//console.log(area);
-alert("The area of rectanlge is " + area + " sq feet");
+
+prompt = ("test, 1,2,3 ");
