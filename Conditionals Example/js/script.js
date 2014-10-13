ef6a3c709a6246ec36d86effc522e2eede8f1f35
varick <varick@fullsail.edu>
2014-10-13 16:45:08 -0400

    Conditional Logic
    Conditional Logic

---
+++ Conditionals Example/js/script.js
@@ -0,0 +1,13 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Conditional Logic
+ 10-13-2014
+ */
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride"!
+if(){
+
+}
---
+++ Expressions_worksheet/Slice of Pie part 1/js/slice_of_pie.js
@@ -0,0 +1,10 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Slice of Pie
+ 10-09-2014
+ */
+//Welcome
+
+alert ("test 1,2,3");
+
