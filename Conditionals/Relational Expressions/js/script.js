f6aba63c9570d9a2b28c22697f7cd8fdb437e36c
varick <varick@fullsail.edu>
2014-10-13 17:01:36 -0400

    Finished Relational Expressions
    Finished Relational Expressions

--- Conditionals/Relational Expressions/js/script.js
+++ Conditionals/Relational Expressions/js/script.js
@@ -6,10 +6,11 @@
  */
 
 var kidHeight = 50;
+var minHeight = 48
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > 48){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!")
 }
