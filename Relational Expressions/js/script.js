3c5ad53c8ce635db2de5f13d6b7e0a9c66f59cba
varick <varick@fullsail.edu>
2014-10-13 16:50:41 -0400

    Relational Expressions
    Relational Expressions

---
+++ Relational Expressions/js/script.js
@@ -0,0 +1,14 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Conditional Logic
+ 10-13-2014
+ */
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride"!
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
