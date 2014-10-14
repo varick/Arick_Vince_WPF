994762f719153e609b20742db77c82a3672f2ae5
varick <varick@fullsail.edu>
2014-10-14 15:58:16 -0400

    Movie Ticket Price age
    Movie Ticket Price age

--- Conditionals_Worksheet/Movie Ticket Price/js/script.js
+++ Conditionals_Worksheet/Movie Ticket Price/js/script.js
@@ -7,5 +7,17 @@
 
 //Time of Movie
 //Age Customer
+//Ticket price $12.00 but if 55 and Older, or under 10 price is $7
 
-//printout The tickets price is x
+var childDiscount = (10)
+var seniorDiscount = (55)
+
+//Figure age with price
+var discount = prompt ("How old are you?");
+    if (childDiscount > discount || seniorDiscount < discount){
+    alert ("Ticket price $7");
+    }else{
+    alert ("Ticket price $12.");
+    }
+
+//Figure time with price
