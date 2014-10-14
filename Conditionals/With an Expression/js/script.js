54b5a248e348f9ce2281a8ecdf982aeb7b91d44a
varick <varick@fullsail.edu>
2014-10-13 20:09:07 -0400

    With an Expression
    With an Expression Finished

--- Conditionals/With an Expression/js/script.js
+++ Conditionals/With an Expression/js/script.js
@@ -5,12 +5,13 @@ With an Expression
  10-13-2014
  */
 
-var kidHeight = 50;
-var minHeight = 48
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!")
 }
