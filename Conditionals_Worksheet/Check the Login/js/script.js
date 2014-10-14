e7545c3151b2ab848073423ca3669569c26c6998
varick <varick@fullsail.edu>
2014-10-14 12:12:09 -0400

    Setup Check Login
    Setup Check Login

--- Conditionals_Worksheet/Check the Login/js/script.js
+++ Conditionals_Worksheet/Check the Login/js/script.js
@@ -1,24 +1,21 @@
 /*
  Vince Arick
  WPF 1410 Section 01
- Last Chance for Gas!
+ Check Login
  10-14-2014
  */
 
-//Gas efficiency of the car(mpg)
-//Gauge reading of the gas tank (in%)
-//Car's gas tank capacity (in gallons)
-var gasEfficiency = 20
-var gaugeReading = 30
-var gasTank = 23
+//Username entered by user
+//Password entered by user
+//Correct username
+//Correct password
 
-//full tank can go 460 miles
-
-if (gaugeReading >= 40 && gasEfficiency >= 20 && gasTank >= 23){
-    console.log("Yes, you can make it without stopping for gas!")
-}else{
-    console.log("You only have X gallons of gas in your tank, better get gas now while you can!")
-}
+var userName = John Smith
+var userPassword = 123456
 
 
-//Print "Yes, you can make it without stopping for gas!" or "You only have X gallons of gas in your tank, better get gas now
+
+//Printout
+//“Welcome, (place their username here)!”  - if the username and password is correct
+//“User not found. Try again.” -if the username does not match
+//“Password does not match our records.”  -if the username matches but the password does not
