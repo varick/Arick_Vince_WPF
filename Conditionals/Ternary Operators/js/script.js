a541fc8ff5c924028f1ea75a82a57696cb34830a
varick <varick@fullsail.edu>
2014-10-13 22:06:30 -0400

    Child age for book
    Child age for book

--- Conditionals/Ternary Operators/js/script.js
+++ Conditionals/Ternary Operators/js/script.js
@@ -5,12 +5,5 @@
  10-13-2014
  */
 
-var gpa = 48;
-
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if(gpa > 2.0){
-    console.log("You can graduate!")
-}else{
-    console.log("GPA is too low!")
-}*/
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+var age = 11;
+var book;
