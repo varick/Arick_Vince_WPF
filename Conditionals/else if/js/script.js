69bc470e35596c9c8917690e9e865627f41e5ca8
varick <varick@fullsail.edu>
2014-10-13 20:41:46 -0400

    Added ride with parent
    Added ride with parent

--- Conditionals/else if/js/script.js
+++ Conditionals/else if/js/script.js
@@ -7,13 +7,17 @@ else if
 
 var kidHeight = 47;
 var minHeight = 48;
-
+var parentHeight = 45; // the height with parent
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride!
     console.log("You can ride the coaster!")
+}else if(kidHeight > parentHeight ){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.")
 }else{
+    //sorry you have growing to do
     console.log("Sorry kid you've got some growing up to do.")
 }
