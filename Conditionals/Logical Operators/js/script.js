b53886dcb73b2a04a67fec526c4c83df18b1ce38
varick <varick@fullsail.edu>
2014-10-13 21:49:32 -0400

    Making or ||
    Making or ||

--- Conditionals/Logical Operators/js/script.js
+++ Conditionals/Logical Operators/js/script.js
@@ -5,11 +5,11 @@
  10-13-2014
  */
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!")
