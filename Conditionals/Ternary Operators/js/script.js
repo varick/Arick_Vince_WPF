b506e46032d844e40613f364985052524a6dabe1
varick <varick@fullsail.edu>
2014-10-13 22:17:44 -0400

    Ternary Operators Final
    Ternary Operators Final

--- Conditionals/Ternary Operators/js/script.js
+++ Conditionals/Ternary Operators/js/script.js
@@ -5,5 +5,9 @@
  10-13-2014
  */
 
-var age = 11;
+var age = 6;
 var book;
+
+//If the child is under 10, they get Green Eggs and Ham otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
