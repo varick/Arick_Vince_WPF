56022261acce71dc1277f872533b75257a90131c
varick <varick@fullsail.edu>
2014-10-14 11:41:44 -0400

    Print out if will make it
    Print out if will make it

--- Conditionals_Worksheet/Last Chance for Gas!/js/script.js
+++ Conditionals_Worksheet/Last Chance for Gas!/js/script.js
@@ -5,3 +5,20 @@
  10-14-2014
  */
 
+//Gas efficiency of the car(mpg)
+//Gauge reading of the gas tank (in%)
+//Car's gas tank capacity (in gallons)
+var gasEfficiency = 20
+var gaugeReading = 30
+var gasTank = 23
+
+//full tank can go 460 miles
+
+if (gaugeReading > 40 ){
+    console.log("Yes, you can make it without stopping for gas!")
+}else{
+    console.log("You only have X gallons of gas in your tank, better get gas now while you can!")
+}
+
+
+//Print "Yes, you can make it without stopping for gas!" or "You only have X gallons of gas in your tank, better get gas now
