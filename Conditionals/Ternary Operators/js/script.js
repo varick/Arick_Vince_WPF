4f9cc1f7f095fb0b2aa14f0438c24969f77d090f
varick <varick@fullsail.edu>
2014-10-13 22:00:27 -0400

    Ternary setup
    Ternary setup

---
+++ Conditionals/Ternary Operators/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Ternary
+ 10-13-2014
+ */
+
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+if(gpa > 2.0){
+    console.log("You can graduate!")
+}else{
+    console.log("GPA is too low!")
+}
