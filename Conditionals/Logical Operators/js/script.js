b26ec461940cd35637e7f8315686fc00356bc46e
varick <varick@fullsail.edu>
2014-10-13 21:08:32 -0400

    Prints 2 answers
    Prints 2 answers

--- Conditionals/Logical Operators/js/script.js
+++ Conditionals/Logical Operators/js/script.js
@@ -5,5 +5,19 @@
  10-13-2014
  */
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
+//if the price of the is less our budget AND if our paycheck is over 300
+//if the price of the phone is less our budget
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
+if(paycheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!")
 }
