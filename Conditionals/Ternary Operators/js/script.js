e36011da366a10c29a8cb4a451b50b3930228084
varick <varick@fullsail.edu>
2014-10-13 22:03:39 -0400

    GPA graduate
    GPA graduate

--- Conditionals/Ternary Operators/js/script.js
+++ Conditionals/Ternary Operators/js/script.js
@@ -8,8 +8,9 @@
 var gpa = 48;
 
 //if the gpa is over the min 2.0 score, the student can graduate
-if(gpa > 2.0){
+/*if(gpa > 2.0){
     console.log("You can graduate!")
 }else{
     console.log("GPA is too low!")
-}
+}*/
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
