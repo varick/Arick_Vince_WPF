e9ee13d17839dbf140dac1882d42c3025aff65eb
varick <varick@fullsail.edu>
2014-10-13 20:02:21 -0400

    With an Expression
    With an Expression

---
+++ Conditionals/With an Expression/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+With an Expression
+ 10-13-2014
+ */
+
+var kidHeight = 50;
+var minHeight = 48
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!")
+}
