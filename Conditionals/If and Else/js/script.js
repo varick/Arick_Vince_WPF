109439c3fa9ba2a92539ba4ce6e05c60eb50033a
varick <varick@fullsail.edu>
2014-10-13 20:20:22 -0400

    If and Else
    If and Else

---
+++ Conditionals/If and Else/js/script.js
@@ -0,0 +1,20 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+If and Else
+ 10-13-2014
+ */
+
+var kidHeight = 47;
+var minHeight = 48;
+
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!")
+}
+if (kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first!")
+}
