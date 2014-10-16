bb7805f636f3080fefa99dd5ef5e9488f5ec4c87
varick <varick@fullsail.edu>
2014-10-16 09:22:22 -0400

    finished conditional industry
    finished conditional industry

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -8,18 +8,23 @@
 //Should you make a responsive website, or fixed
 
 //Ask responsive design question
-alert ("Let's check to see if you should make a responsive design website.")
+alert ("Let's check to see if you should make a responsive, or fixed design website.");
 //prompt how many devices
-var mobile = prompt ("How many mobile devices do you have?")
+var mobile = prompt ("How many mobile devices do you have?");
+//Print answer
 console.log (mobile)
+//if they have 1 prompt
 if (mobile == 1) {
-    alert("You may want to think about making Responsive design website")
+    alert("You may want to think about making Responsive design website");
+//validate
 }else if (mobile === "") {
     mobile = prompt("Please enter amount don't leave it blank");
+//if you have more than 1
 }else if (mobile >= 2){
-    alert("You should make a responsive design website.")
+    alert("You should make a responsive design website.");
+//if you have 0
 }else{
-    alert("You probably have one you just don't know it.")
+    alert("You probably have one you just don't know it.");
 }
 
 
