f1013ff9adfbbf64ec802fa7e662d584e3123f51
varick <varick@fullsail.edu>
2014-10-16 10:51:36 -0400

    How many cashiers and items
    How many cashiers and items

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -9,18 +9,14 @@
 
 //Waiting in line at grocery store
 
-/*var cashier = 2
-var selfCheck = 1
- */
-
 
 //Which would be quicker to leave the store self check out or cashier
 //How many items do I have
 var items = prompt ("How many items do you have?")
-console.log items
+    console.log (items);
 var cashier = prompt ("How many cashiers are there?")
-console.log cashier
-if (items >= 10 && cashier <= 4){
+    console.log (cashier);
+if (items >= "10" && cashier <= "4"){
     alert ("It might be quicker to go self check out.")
 }else{
     ("Wait in line shouldn't take to long.")
