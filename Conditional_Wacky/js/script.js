cef0f90a9193eff5a39cf0e19ab111be206eb98a
varick <varick@fullsail.edu>
2014-10-16 13:30:12 -0400

    Corrected validation on conditional wacky
    Corrected validation on conditional wacky

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -16,18 +16,22 @@ alert ("You are at the grocery store, and you aren't sure to go to self checkout
 var customers = prompt ("How many people waiting before you in self checkout?");
     console.log (customers);
 //validate
-}else if (customers === ""){
-    customers = prompt("Please enter amount don't leave it blank");
+if (customers === ""){
+    customers = prompt ("You must enter a number.")
+}
+console.log (customers);
 //How many cashiers are there?
 var cashier = prompt ("How many cashiers are there?");
     console.log (cashier);
 //validate
-}else if (cashier === ""){
-    cashier = prompt("Please enter amount don't leave it blank");
+if (cashier === ""){
+    cashier = prompt ("You must enter a number.")
+}
+console.log (cashier);
 //If you have less than 10 items and there greater or equal to 1 cashier quicker to go self check out
 //If you have more than 10 items and less or equal to 1
 if (customers <= 5 && cashier >=2){
     alert ("Wait in line for a cashier should be faster.")
 }else{
-    alert ("Go self checkout.")
+    alert ("Go to self checkout.")
 }
