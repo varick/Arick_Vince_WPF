6d46b712d767278be60a236a0b073ed8a5d31839
varick <varick@fullsail.edu>
2014-10-16 09:03:00 -0400

    Setup Conditional Industry
    Setup Conditional Industry

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -1,32 +1,22 @@
 /*
  Vince Arick
  WPF 1410 Section 01
- Check Login
- 10-14-2014
+ Conditional Industry
+ 10-16-2014
  */
 
-//correct name and password
-var correctName = ("Vince")
-var correctPassword = ("123456")
+//Should you make a responsive website, or fixed
 
-//ask what the user name is and if correct ask for password
-var correctUser  = prompt ("What is your username?");
 
-//Correct name, ask for password and save password
-if (correctName === correctUser){
-    var passWord = prompt ("What is your password?");
-console.log(passWord)
 
-//Wrong user name leave
+var mobile = prompt ("How many mobile devices do you have?")
+
+if (mobile == 1) {
+    alert("You may want to think about making Responsive design website")
+}else if (mobile >= 2){
+    alert("You should make a responsive design website.")
 }else{
-    alert ("Username does not match our records.");
+    alert("You probably have one you just don't know it.")
 }
-console.log(passWord)
 
-//ask for correct password, if don't match leave
-if (passWord === correctPassword){
-    alert("Welcome " + correctName);
-}else{
-    alert("Password does not match our records.");
-}
 
