ec080871c5766cb54e8924b3adecbac15a6d0314
varick <varick@fullsail.edu>
2014-10-16 09:10:36 -0400

    Creating Comments in conditional industry
    Creating Comments in conditional industry

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -7,12 +7,15 @@
 
 //Should you make a responsive website, or fixed
 
-
-
+//Ask responsive design question
+alert ("Let's check to see if you should make a responsive design website.")
+//prompt how many devices
 var mobile = prompt ("How many mobile devices do you have?")
-
+console.log (mobile)
 if (mobile == 1) {
     alert("You may want to think about making Responsive design website")
+}else if (mobile === "") {
+    mobile = prompt("Please enter amount don't leave it blank");
 }else if (mobile >= 2){
     alert("You should make a responsive design website.")
 }else{
