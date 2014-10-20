255575483105191619b1fe1fba77aba42559204f
varick <varick@fullsail.edu>
2014-10-20 16:46:29 -0400

    Returning Values finished
    Returning Values finished

--- Week 4 Screencast/Returning Values/js/script.js
+++ Week 4 Screencast/Returning Values/js/script.js
@@ -1,37 +1,14 @@
 /*
  Vince Arick
  WPF 1410 Section 01
- Conditional Wacky
- 10-16-2014
+ Returning Values
+ 10-20-2014
  */
 
+var total = calcArea(30, 20);
 
-
-//Waiting in line at grocery store
-
-
-//Which would be quicker to leave the store self check out or cashier
-alert ("You are at the grocery store, and you aren't sure to go to self checkout or cashier on how many customers are there?");
-//How many items do I have?
-var customers = prompt ("How many people waiting before you in self checkout?");
-    console.log (customers);
-//validate
-if (customers === ""){
-    customers = prompt ("You must enter a number.")
+function calcArea(w, h){
+    var area = w * h;
+    return area; //function spitting the info out
 }
-console.log (customers);
-//How many cashiers are there?
-var cashier = prompt ("How many cashiers are there?");
-    console.log (cashier);
-//validate
-if (cashier === ""){
-    cashier = prompt ("You must enter a number.")
-}
-console.log (cashier);
-//If you have less than 10 items and there greater or equal to 1 cashier quicker to go self check out
-//If you have more than 10 items and less or equal to 1
-if (customers <= 5 && cashier >=2){
-    alert ("Wait in line for a cashier should be faster.")
-}else{
-    alert ("Go to self checkout.")
-}
+console.log(total);
