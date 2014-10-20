52456e67ebd8ad5a96ead9d1dab87f1371bab53e
varick <varick@fullsail.edu>
2014-10-20 16:06:27 -0400

    Basic Structure Screencast
    Basic Structure Screencast

---
+++ Week 4 Screencast/Basic Function Structure/js/script.js
@@ -0,0 +1,17 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Basic Structure
+ 10-20-2014
+ */
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
