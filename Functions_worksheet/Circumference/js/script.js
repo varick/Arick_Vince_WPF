24613f3d2fef10ab0c9007bc2fbd54ec43326e0a
varick <varick@fullsail.edu>
2014-10-20 21:04:27 -0400

    Circumference of circle - setup
    Circumference of circle - setup

---
+++ Functions_worksheet/Circumference/js/script.js
@@ -0,0 +1,12 @@
+/*
+ Vince Arick
+ WPF 1410 Section 01
+ Circumference
+ 10-20-2014
+ */
+
+//circumference = 2 x 3.141 x r
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
