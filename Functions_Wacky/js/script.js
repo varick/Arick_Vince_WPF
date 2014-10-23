37ac68d0b2c7b6562644add1e99523515623f0b1
varick <varick@fullsail.edu>
2014-10-23 14:30:32 -0400

    Wacky function lottery numbers
    Wacky function lottery numbers

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,22 +5,27 @@
  10-23-2014
  */
 
-
+//Let's play the lottery
 var min = alert ("Lets play the lottery 5 balls and 1 mega 1-54!");
 
+//pick random number to display mega number 1 to 54
 var result=randomizer(1,54);
 
+//display mega number
 alert ("The mega number is " + result)
 
-
+//function random arguement
 function randomizer(minNumber,maxNumber){
 
+//Math round number multiply max number - min number + min number
     var randomNumber = Math.round(Math.random()*(maxNumber-minNumber)+ Number(minNumber));
 
+//return random number
     return randomNumber;
 }
-
+//loop 5 times to get 5 random numbers
 for (var i=0; i<5; i++) {
 
-    alert(randomizer(1,54));
+//alert 5 winning number
+    alert ("Winning numbers are " + randomizer(1,54));
 }
