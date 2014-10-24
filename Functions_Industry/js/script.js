e0360bd6275b326fa343a7e6ebc2f0b73ae57463
varick <varick@fullsail.edu>
2014-10-23 21:16:37 -0400

    Functions Industry-setup
    Functions Industry-setup

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,6 +5,8 @@
  10-23-2014
  */
 
+var size = prompt("What size of your canvas?");
+var images = prompt("How many images do you want to use?");
 
-}
 
+console.log (size / images);
--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,5 +6,15 @@
  */
 
 
-}
+//We want to fit x amount of images with y images on a 960 width
+alert ("What size images do you need to evenly fit 960 width?");
 
+var images = prompt ("How many images do you want to fit into your call to action?");
+
+var padding = prompt ("What size padding would you like in between images?");
+
+var width = prompt ("What's the width of the call to action banner?");
+
+var subImages = (width / images);
+
+console.log subImages
