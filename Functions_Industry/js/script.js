0e3dbe856ff2de0aa558c96ae8fe51538b6415e7
varick <varick@fullsail.edu>
2014-10-23 21:41:30 -0400

    Functions Industry-Final
    Functions Industry-Final

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,8 +5,17 @@
  10-23-2014
  */
 
-var size = prompt("What size of your canvas?");
+//What size is the canvas in pixels?
+var size = prompt("What is the size of your canvas in pixels?");
+//How many images to put in your image?
 var images = prompt("How many images do you want to use?");
-
-
-console.log (size / images);
+//How much padding would you like between image?
+var padding = prompt("How much padding in pixels?")
+//Size of your canvas divided by how many images?
+var totalImages = (size / images);
+//How many spaces will there be between images
+var spaces = (images - 1);
+//Multiply how many spaces to padding
+var totalPadding = (spaces * padding);
+//Alert total pixel width of images should be
+alert ("Images should be " + (totalImages - totalPadding) + " pixels wide." );
