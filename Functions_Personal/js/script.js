1cefefea9991fe4f9b3c9cc70731cabccccdbb8e
varick <varick@fullsail.edu>
2014-10-23 22:48:37 -0400

    Function Personal-Final
    Function Personal-Final

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,11 +4,23 @@
  Functions Personal
  10-23-2014
  */
+
+
 var myChildren = 250
 
 var budget = prompt("How much would you like to spend for Christmas?");
+
+alert ("We already set aside $250 for the kids");
 
 var family = prompt("How many family and friends would you like to give presents?");
 
 var equalAmount = alert ("Equal amount of money per person is " + (budget - myChildren) / (family) + " dollars.");
 //if you want to give some bigger presents
+
+var bonus = prompt ("Do you expect a bonus, if so how much?");
+
+if (bonus >= myChildren){
+    alert ("Everyone is going to get a little extra this month.");
+}else{
+    alert ("Scrooge screwed us nothing special this year.");
+}
